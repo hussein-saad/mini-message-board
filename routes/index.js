b27e14ded47c63003302c7f408d806fa324ca995
HusseinSaad <hussein.saad2021@gmail.com>
2023-11-07 21:29:49 +0200

    Add a form massaging page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const messages = [];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
 });
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "Mini Messageboard" });
+});
+
+router.post("/new", function (req, res, next) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.author,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
+
+
+
 
 module.exports = router;
